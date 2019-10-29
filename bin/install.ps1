c89d87b5ab02a47ff07d7125d727abaf1c0c2428
Koyashiro Kohaku <6698252+KoyashiroKohaku@users.noreply.github.com>
2019-10-29 20:58:15 +0900

    separate .gitignore.user from .gitignore

--- bin/install.ps1
+++ bin/install.ps1
@@ -9,6 +9,17 @@ if (Test-Path $link) {
 Write-Host "mklink $target > $link"
 powershell.exe -Command Start-Process -FilePath "cmd" -ArgumentList "/c", "mklink", $link, $target -Verb Runas
 
+$link = "${env:USERPROFILE}\.gitconfig.user"
+$target = "${env:USERPROFILE}\dotfiles\.gitconfig.user"
+
+if (Test-Path $link) {
+  Write-Host "remove > $link"
+  Remove-Item $link
+}
+
+Write-Host "mklink $target > $link"
+powershell.exe -Command Start-Process -FilePath "cmd" -ArgumentList "/c", "mklink", $link, $target -Verb Runas
+
 $link = "${env:USERPROFILE}\_vimrc"
 $target = "${env:USERPROFILE}\dotfiles\.vimrc"
 
