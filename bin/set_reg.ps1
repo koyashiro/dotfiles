892f44f635507b1963b4ed92d4e30e993d679886
Koyashiro Kohaku <develop@koyashiro.net>
2020-08-07 18:26:13 +0900

    chore: update set_reg.ps1

--- bin/set_reg.ps1
+++ bin/set_reg.ps1
@@ -1,4 +1,4 @@
 # Windows Terminal
 REG ADD "HKEY_CLASSES_ROOT\Directory\Background\shell\WindowsTerminal" /t REG_SZ /d "Open Windows Terminal here" /f
 REG ADD "HKEY_CLASSES_ROOT\Directory\Background\shell\WindowsTerminal" /v "Icon" /t REG_SZ /d "${env:USERPROFILE}\icons\terminal.ico" /f
-REG ADD "HKEY_CLASSES_ROOT\Directory\Background\shell\WindowsTerminal\command" /t REG_SZ /d "${env:LOCALAPPDATA}\Microsoft\WindowsApps\wt.exe" /f
+REG ADD "HKEY_CLASSES_ROOT\Directory\Background\shell\WindowsTerminal\command" /t REG_SZ /d "${env:LOCALAPPDATA}\Microsoft\WindowsApps\wt.exe -d %V" /f
