cecdbe95dcbb22b07a9b18c875bdb05572848b34
Koyashiro Kohaku <develop@koyashiro.net>
2020-08-09 21:09:09 +0900

    rename: PowerShell/Microsoft.PowerShell_profile.ps1 => PowerShell/profile.ps1

--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -1,14 +1,6 @@
 $rawui = (Get-Host).UI.RawUI
 $rawui.WindowTitle = "PowerShell 7"
 
-Remove-Item alias:ls
-Remove-Item alias:pwd
-Remove-Item alias:clear
-
-function ll {
-  ls -la
-}
-
 Import-Module posh-git
 Import-Module oh-my-posh
 Set-Theme Paradox
