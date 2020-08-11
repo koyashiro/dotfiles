a87f5239922bd8a1e4a3388cfc647008b265d08c
Koyashiro Kohaku <develop@koyashiro.net>
2020-08-11 12:05:17 +0900

    fix: run in PowerShell 7

--- bin/install.ps1
+++ bin/install.ps1
@@ -1,3 +1,3 @@
 Start-Process pwsh -ArgumentList "$PSScriptRoot\symlink.pa1" -Verb runas
-Invoke-Expression -Command $PSScriptRoot\cp.ps1
+Start-Process pwsh -ArgumentList "$PSScriptRoot\cp.ps1"
 Start-Process pwsh -ArgumentList "$PSScriptRoot\registry.ps1" -Verb runas
