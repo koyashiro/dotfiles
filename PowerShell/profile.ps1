02d279432c83d8ac65e86b3a3612946a90977f16
Koyashiro Kohaku <develop@koyashiro.net>
2020-08-28 11:51:43 +0900

    feat: add PATH and aliases

--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -1,6 +1,13 @@
 $rawui = (Get-Host).UI.RawUI
 $rawui.WindowTitle = "PowerShell 7"
 
+$ENV:Path = "${HOME}\bin;" + $ENV:Path
+
+Set-Alias clr Clear-Host
+Set-Alias g git
+Set-Alias d docker
+Set-Alias dc docker-compose
+
 Import-Module posh-git
 Import-Module oh-my-posh
 Set-Theme Paradox
