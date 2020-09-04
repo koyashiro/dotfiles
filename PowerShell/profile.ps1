3e586d033beba08e2d6e9f159f7692f9d8af4839
Koyashiro Kohaku <develop@koyashiro.net>
2020-09-04 12:22:05 +0900

    chore: update alias

--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -3,10 +3,27 @@ $rawui.WindowTitle = "PowerShell 7"
 
 $ENV:Path = "${HOME}\bin;" + $ENV:Path
 
+Remove-Alias ls
+Remove-Alias cat
+Remove-Alias echo
+Remove-Alias cp
+Remove-Alias mv
+Remove-Alias rm
+Remove-Alias pwd
+
 Set-Alias clr Clear-Host
 Set-Alias g git
 Set-Alias d docker
 Set-Alias dc docker-compose
+
+
+function ll {
+  ls -lh
+}
+
+function la {
+  ls -lhA
+}
 
 Import-Module posh-git
 Import-Module oh-my-posh
