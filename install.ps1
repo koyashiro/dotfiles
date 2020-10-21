805d56c01021dd4934ee0d6a7e28395045445a28
Koyashiro Kohaku <develop@koyashiro.net>
2020-10-22 00:47:24 +0900

    feat: update `install.ps1`

--- install.ps1
+++ install.ps1
@@ -9,32 +9,59 @@ if (!(Get-Module oh-my-posh)) {
 }
 
 # exclude dotfiles
-$excludes=@(
-    '.git',
-    '.gitattributes',
-    '.gitconfig.user'
+$excludes = @(
+  '.git',
+  '.gitattributes',
+  '.gitconfig.user',
+  '.wsl'
 )
 
 # dotfiles
 foreach ($f in Get-ChildItem -Name .*) {
-    if ($f -in $excludes) {
-      continue
-    }
+  if ($f -in $excludes) {
+    continue
+  }
 
-    New-Item -ItemType SymbolicLink -Value $(Convert-Path $f) -Path $(Join-Path $HOME $f) -Force
+  New-Item -ItemType SymbolicLink -Value $(Convert-Path $f) -Path $(Join-Path $HOME $f) -Force
 }
 
 # rename .vimrc -> _vimrc
 if (Test-Path $HOME\.vimrc) {
-    Move-Item -Force $HOME\.vimrc $HOME\_vimrc
+  Move-Item -Force $HOME\.vimrc $HOME\_vimrc
 }
 
 # Windows Terminal
 $localAppDataDir = 'win\AppData\Local'
 $f = 'Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json'
-New-Item -ItemType SymbolicLink -Value $(Convert-Path $(Join-Path $localAppDataDir $f)) -Path $(Join-Path $env:LOCALAPPDATA $f) -Force
+New-Item `
+  -ItemType SymbolicLink `
+  -Force `
+  -Value $(Convert-Path $(Join-Path $localAppDataDir $f)) `
+  -Path $(Join-Path $env:LOCALAPPDATA $f)
 
-# PowerShell
+# Windows Package Manager
+$localAppDataDir = 'win\AppData\Local'
+$f = 'Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json'
+New-Item `
+  -ItemType SymbolicLink `
+  -Force `
+  -Value $(Convert-Path $(Join-Path $localAppDataDir $f)) `
+  -Path $(Join-Path $env:LOCALAPPDATA $f)
+
+# Windows PowerShell
+$documentsDir = 'win\Documents'
+$f = 'WindowsPowerShell\profile.ps1'
+New-Item `
+  -ItemType SymbolicLink `
+  -Force `
+  -Value $(Convert-Path $(Join-Path $documentsDir $f)) `
+  -Path $(Join-Path $([Environment]::GetFolderPath('Personal')) $f) `
+
+# PowerShell 7
 $documentsDir = 'win\Documents'
 $f = 'PowerShell\profile.ps1'
-New-Item -ItemType SymbolicLink -Value $(Convert-Path $(Join-Path $documentsDir $f)) -Path $profile.CurrentUserAllHosts -Force
+New-Item `
+  -ItemType SymbolicLink `
+  -Force `
+  -Value $(Convert-Path $(Join-Path $documentsDir $f)) `
+  -Path $(Join-Path $([Environment]::GetFolderPath('Personal')) $f) `
