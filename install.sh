9aea96b02a523e1c56311c20d453c544adee8ebd
Koyashiro Kohaku <develop@koyashiro.net>
2020-10-31 06:03:18 +0900

    refactor: update `install.sh`

--- install.sh
+++ install.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+# dot files
 for f in .??*; do
+  [[ "$f" == '.config' ]] && continue
   [[ "$f" == '.git' ]] && continue
   [[ "$f" == '.gitattributes' ]] && continue
   [[ "$f" == '.gitconfig.user' ]] && continue
-  [[ "$f" == '.wsl' ]] && [[ ! -f /proc/sys/fs/binfmt_misc/WSLInterop ]] && continue
+  [[ "$f" == '.wsl' ]] && continue
 
   ln -fns "$(cd $(dirname $f) && pwd)/$(basename $f)" "$HOME/$f"
 done
+
+# $XDG_CONFIG_HOME or .config
+for f in .config/??*; do
+  ln -fns "$(cd $(dirname $f) && pwd)/$(basename $f)" "${XDG_CONFIG_HOME:-$HOME/.cache}/$(basename $f)"
+done
+
+# wsl
+if [[ -f /proc/sys/fs/binfmt_misc/WSLInterop ]]; then
+  ln -fns "$(cd $(dirname .wsl) && pwd)/$(basename .wsl)" $HOME/.wsl
+fi
