b2839684c637cbb7ff3f71f60f5aa577010644f2
Koyashiro Kohaku <develop@koyashiro.net>
2020-10-31 10:56:48 +0900

    style: format

--- .config/zsh/rc/alias.zsh
+++ .config/zsh/rc/alias.zsh
@@ -2,10 +2,10 @@ alias ..='cd ..'
 
 # enable color support
 if [ -x /usr/bin/dircolors ]; then
-    alias ls='ls --color=auto'
-    alias grep='grep --color=auto'
-    alias fgrep='fgrep --color=auto'
-    alias egrep='egrep --color=auto'
+  alias ls='ls --color=auto'
+  alias grep='grep --color=auto'
+  alias fgrep='fgrep --color=auto'
+  alias egrep='egrep --color=auto'
 fi
 
 # ls
