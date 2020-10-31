23efd5a102ab4c84154d069ae5e41f05c15d7b86
Koyashiro Kohaku <develop@koyashiro.net>
2020-10-31 09:20:17 +0900

    fix: add existence check for `powerline-shell`

--- .config/bash/prompt.bash
+++ .config/bash/prompt.bash
@@ -1,7 +1,9 @@
-function _update_ps1() {
-  PS1=$(powerline-shell $?)
-}
+if type powerline-shell >/dev/null 2>&1; then
+  function _update_ps1() {
+    PS1=$(powerline-shell $?)
+  }
 
-if [[ $TERM != linux && ! $PROMPT_COMMAND =~ _update_ps1 ]]; then
-  PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
+  if [[ $TERM != linux && ! $PROMPT_COMMAND =~ _update_ps1 ]]; then
+    PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
+  fi
 fi
