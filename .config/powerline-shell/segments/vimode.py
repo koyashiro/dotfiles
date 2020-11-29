3dcd2d35d2bdfde3b1531a5d0183cc4670a1343b
Koyashiro Kohaku <develop@koyashiro.net>
2020-11-29 14:16:47 +0900

    feat: add `VIMODE` segment for `powerline-shell`

---
+++ .config/powerline-shell/segments/vimode.py
@@ -0,0 +1,27 @@
+from powerline_shell.utils import BasicSegment
+import os
+
+FG_NORMAL = '0'
+BG_NORMAL = '247'
+FG_INSERT = '0'
+BG_INSERT = '12'
+
+
+class Segment(BasicSegment):
+    def add_to_powerline(self):
+        vimode = os.environ['VIMODE']
+
+        if vimode == 'NORMAL':
+            fg = FG_NORMAL
+            bg = BG_NORMAL
+        elif vimode == 'INSERT':
+            fg = FG_INSERT
+            bg = BG_INSERT
+        else:
+            fg = FG_NORMAL
+            bg = BG_NORMAL
+
+        self.powerline.append(
+            ' %s ' % vimode,
+            fg,
+            bg)
