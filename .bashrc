95af913c04ea501712a3d52fccbf25955db258d9
Koyashiro Kohaku <develop@koyashiro.net>
2020-12-10 13:14:23 +0900

    refactor: remove `function.sh`

--- .bashrc
+++ .bashrc
@@ -6,8 +6,3 @@ fi
 for f in $XDG_CONFIG_HOME/sh/*.sh; do
   source $f
 done
-
-# bash
-for f in $XDG_CONFIG_HOME/bash/*.bash; do
-  source $f
-done
--- .config/sh/function.sh
+++
@@ -1,7 +0,0 @@
-function fe() {
-  local selected=$(fzf-tmux --preview "bat --color=always --style=header,grid --line-range :100 {}")
-
-  if [[ -n "$selected" ]]; then
-    $EDITOR $selected
-  fi
-}
