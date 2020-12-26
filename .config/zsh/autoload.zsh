0e33090ff33900dd168727fd4ac1a6f64078748d
Koyashiro Kohaku <develop@koyashiro.net>
2020-12-27 01:22:13 +0900

    fix: fix `compinit`

--- .config/zsh/autoload.zsh
+++ .config/zsh/autoload.zsh
@@ -1,5 +1,5 @@
 autoload -Uz colors && colors
 autoload -Uz run-help
 autoload -Uz add-zsh-hook
-autoload -Uz compinit
+autoload -Uz compinit && compinit -d "$XDG_DATA_HOME"/zsh/history
 autoload -Uz is-at-least
