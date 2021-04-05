261cd3ce5ee0eea5d82bfd93532c34e96f233e18
koyashiro <develop@koyashiro.net>
2021-04-05 22:57:36 +0900

    feat: add bundle environments

--- .config/sh/env.sh
+++ .config/sh/env.sh
@@ -80,6 +80,9 @@ export YARN_CONFIG="$XDG_CONFIG_HOME"/yarn/yarnrc
 # Ruby
 export GEM_HOME="$XDG_DATA_HOME"/gem
 export GEM_SPEC_CACHE="$XDG_CACHE_HOME"/gem
+export BUNDLE_USER_CONFIG="$XDG_CONFIG_HOME"/bundle
+export BUNDLE_USER_CACHE="$XDG_CACHE_HOME"/bundle
+export BUNDLE_USER_PLUGIN="$XDG_DATA_HOME"/bundle
 export RBENV_ROOT="$XDG_DATA_HOME"/rbenv
 export SOLARGRAPH_CACHE="$XDG_CACHE_HOME"/solargraph/cache
 
