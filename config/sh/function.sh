6ff1a5995d600c568f56cb732d9f28ce43691157
koyashiro <develop@koyashiro.net>
2021-04-14 18:47:09 +0900

    feat: add `pull_all_docker_image` function

--- config/sh/function.sh
+++ config/sh/function.sh
@@ -34,3 +34,7 @@ fzf_git_branch_delete() {
     git branch -d $selecteds
   )
 }
+
+pull_all_docker_image() {
+  docker images | tail -n +2 | awk -v 'OFS=:' '{print $1,$2}' | xargs -P0 -L1 docker pull
+}
