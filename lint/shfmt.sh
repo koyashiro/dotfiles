f072c83852d47e4b6915a11dd517bfbfcf6c85d9
koyashiro <develop@koyashiro.net>
2021-04-30 03:26:08 +0900

    feat: add `shfmt` script

---
+++ lint/shfmt.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+readonly DOTDIR="$HOME"/.dotfiles
+
+cd "$DOTDIR"
+
+# install.sh
+shfmt -d install.sh
+
+# sh
+shfmt -d -p -i 2 profile
+shfmt -d -p -i 2 config/sh/env.sh
+shfmt -d -p -i 2 config/sh/alias.sh
+shfmt -d -p -i 2 config/sh/function.sh
+
+# bash
+shfmt -d -ln bash -i 2 bash_profile
+shfmt -d -ln bash -i 2 bashrc
+
+# zsh
+shfmt -d -i 2 zshenv
+shfmt -d -i 2 config/zsh/.zshenv
+shfmt -d -i 2 config/zsh/.zshrc
