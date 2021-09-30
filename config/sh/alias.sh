12820c44143f2e35eacfe44704431a550c0ec5a7
koyashiro <develop@koyashiro.net>
2021-09-30 21:52:08 +0900

    refactor: update docker compose alias

--- config/sh/alias.sh
+++ config/sh/alias.sh
@@ -80,11 +80,11 @@ alias gcp='git cherry-pick'
 alias d=docker
 
 # Docker Compose
-alias dc=docker-compose
-alias dcb='docker-compose build'
-alias dcu='docker-compose up'
-alias dcud='docker-compose up --detach'
-alias dcd='docker-compose down'
-alias dcr='docker-compose run --rm'
-alias dce='docker-compose exec'
-alias dcp='docker-compose pull'
+alias dc='docker compose'
+alias dcb='docker compose build'
+alias dcu='docker compose up'
+alias dcud='docker compose up --detach'
+alias dcd='docker compose down'
+alias dcr='docker compose run --rm'
+alias dce='docker compose exec'
+alias dcp='docker compose pull'
