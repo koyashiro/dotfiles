edc3261f3fe95eee8a86612624db74134d10df70
koyashiro <develop@koyashi.ro>
2022-01-06 23:11:56 +0900

    chore: update `install.sh`

--- install.sh
+++ install.sh
@@ -2,147 +2,123 @@
 
 set -eu
 
-readonly DEFAULT_DOTDIR="$HOME"/.dotfiles
-if [ -z "${DOTDIR:-}" ]; then
-  DOTDIR="$DEFAULT_DOTDIR"
-fi
+DOTDIR="$(cd "$(dirname "$0")"; pwd)"
 
-readonly DEFAULT_REPO_URL=https://github.com/koyashiro/dotfiles
-if [ -z "${REPO_URL:-}" ]; then
-  REPO_URL="$DEFAULT_REPO_URL"
-fi
+is_darwin() {
+  [ "$(uname)" = Darwin ]
+}
 
-readonly DEFAULT_ARCHIVE_URL="$REPO_URL"/archive/main.tar.gz
-if [ -z "${ARCHIVE_URL:-}" ]; then
-  ARCHIVE_URL="$DEFAULT_ARCHIVE_URL"
-fi
+is_wsl() {
+  [ -n "${WSL_INTEROP:-}" ]
+}
 
-readonly DEFAULT_XDG_CONFIG_HOME="$HOME"/.config
-if [ -z "${XDG_CONFIG_HOME:-}" ]; then
-  XDG_CONFIG_HOME="$DEFAULT_XDG_CONFIG_HOME"
-fi
+set_variables() {
+  # DISTRO
+  if [ -r /etc/os-release ]; then
+    # shellcheck source=/etc/os-release
+    DISTRO="$(. /etc/os-release && echo "$NAME")"
+  else
+    DISTRO='(UNKNOWN)'
+  fi
 
-readonly DEFAULT_XDG_DATA_HOME="$HOME"/.local/share
-if [ -z "${XDG_DATA_HOME:-}" ]; then
-  XDG_DATA_HOME="$DEFAULT_XDG_DATA_HOME"
-fi
+  # IS_DARWIN
+  if is_darwin; then
+    IS_DARWIN=true
+  else
+    IS_DARWIN=false
+  fi
 
-if [ -r /etc/os-release ]; then
-  DISTRO="$(. /etc/os-release && echo "$NAME")"
-else
-  DISTRO='(UNKNOWN)'
-fi
+  # IS_WSL
+  if is_wsl; then
+    IS_WSL=true
+  else
+    IS_WSL=false
+  fi
+}
 
-if [ "$(uname)" = Darwin ]; then
-  IS_DARWIN=true
-else
-  IS_DARWIN=false
-fi
-
-if [ -n "${WSL_INTEROP:-}" ]; then
-  IS_WSL=true
-else
-  IS_WSL=false
-fi
-
-cat <<EOF
+show_info() {
+  cat <<EOF
 [install.sh] koyashiro's dotfiles <https://github.com/koyashiro/dotfiles>
 
-[install.sh] (whoami)        : $(whoami)
+[install.sh] USER            : ${USER:-$(whoami)}
 [install.sh] HOME            : $HOME
 [install.sh] DOTDIR          : $DOTDIR
-[install.sh] REPO_URL        : $REPO_URL
-[install.sh] ARCHIVE_URL     : $ARCHIVE_URL
-[install.sh] XDG_CONFIG_HOME : $XDG_CONFIG_HOME
-[install.sh] XDG_DATA_HOME   : $XDG_DATA_HOME
+[install.sh] DISTRO          : $DISTRO
 [install.sh] IS_DARWIN       : $IS_DARWIN
 [install.sh] IS_WSL          : $IS_WSL
 
 EOF
+}
 
-# dotfiles directory checking
-if [ ! -d "$DOTDIR" ]; then
-  # Clone repository by Git
-  if command -v git >/dev/null 2>&1; then
-    echo "[install.sh] git clone $REPO_URL $DOTDIR"
-    git clone "$REPO_URL" "$DOTDIR"
-  # Download archive by curl
-  elif command -v curl >/dev/null 2>&1; then
-    echo "[install.sh] mkdir $DOTDIR"
-    mkdir "$DOTDIR"
-
-    echo "[install.sh] curl -fsSL $ARCHIVE_URL | tar -xz -C $DOTDIR --strip 1"
-    curl -fsSL "$ARCHIVE_URL" | tar -xz -C "$DOTDIR" --strip 1
-  # Download archive by wget
-  elif command -v wget >/dev/null 2>&1; then
-    echo "[install.sh] mkdir $DOTDIR"
-    mkdir "$DOTDIR"
-
-    echo "[install.sh] wget -q -O - $ARCHIVE_URL | tar -xz -C $DOTDIR --strip 1"
-    wget -q -O - "$ARCHIVE_URL" | tar -xz -C "$DOTDIR" --strip 1
-  else
-    echo '[install.sh] error: git, curl or wget required.' 1>&2
-    exit 127
+create_xdg_base_directories() {
+  if [ ! -d "$HOME"/.config ]; then
+    echo "[install.sh] mkdir -m 700 $HOME/.config"
+    mkdir -m 700 "$HOME"/.config
   fi
-fi
 
-# Create XDG Base Directories
-if [ ! -d "$HOME"/.config ]; then
-  echo "[install.sh] mkdir -m 700 $HOME/.config"
-  mkdir -m 700 "$HOME"/.config
-fi
+  if [ ! -d "$HOME"/.cache ]; then
+    echo "[install.sh] mkdir -m 700 $HOME/.cache"
+    mkdir -m 700 "$HOME"/.cache
+  fi
 
-if [ ! -d "$HOME"/.cache ]; then
-  echo "[install.sh] mkdir -m 700 $HOME/.cache"
-  mkdir -m 700 "$HOME"/.cache
-fi
+  if [ ! -d "$HOME"/.local ]; then
+    echo "[install.sh] mkdir -m 700 $HOME/.local"
+    mkdir -m 700 "$HOME"/.local
+  fi
 
-if [ ! -d "$HOME"/.local ]; then
-  echo "[install.sh] mkdir -m 700 $HOME/.local"
-  mkdir -m 700 "$HOME"/.local
-fi
+  if [ ! -d "$HOME"/.local/share ]; then
+    echo "[install.sh] mkdir -m 700 $HOME/.local/share"
+    mkdir -m 700 "$HOME"/.local/share
+  fi
 
-if [ ! -d "$HOME"/.local/share ]; then
-  echo "[install.sh] mkdir -m 700 $HOME/.local/share"
-  mkdir -m 700 "$HOME"/.local/share
-fi
+  if [ ! -d "$HOME"/.local/bin ]; then
+    echo "[install.sh] mkdir -m 700 $HOME/.local/bin"
+    mkdir -m 700 "$HOME"/.local/bin
+  fi
+}
 
-if [ ! -d "$HOME"/.local/bin ]; then
-  echo "[install.sh] mkdir -m 700 $HOME/.local/bin"
-  mkdir -m 700 "$HOME"/.local/bin
-fi
+set_xdg_base_directories() {
+  XDG_CONFIG_HOME="$HOME/.config"
+  # XDG_CACHE_HOME="$HOME/.cache"
+  XDG_DATA_HOME="$HOME/.local/share"
+}
 
-# Create symbolic link
-echo "[install.sh] ln -fns $DOTDIR/profile $HOME/.profile"
-ln -fns "$DOTDIR"/profile "$HOME"/.profile
+create_symbolic_links() {
+  echo "[install.sh] ln -fns $DOTDIR/profile $HOME/.profile"
+  ln -fns "$DOTDIR"/profile "$HOME"/.profile
 
-echo "[install.sh] ln -fns $DOTDIR/bash_profile $HOME/.bash_profile"
-ln -fns "$DOTDIR"/bash_profile "$HOME"/.bash_profile
+  echo "[install.sh] ln -fns $DOTDIR/bash_profile $HOME/.bash_profile"
+  ln -fns "$DOTDIR"/bash_profile "$HOME"/.bash_profile
 
-echo "[install.sh] ln -fns $DOTDIR/bashrc $HOME/.bashrc"
-ln -fns "$DOTDIR"/bashrc "$HOME"/.bashrc
+  echo "[install.sh] ln -fns $DOTDIR/bashrc $HOME/.bashrc"
+  ln -fns "$DOTDIR"/bashrc "$HOME"/.bashrc
 
-echo "[install.sh] ln -fns $DOTDIR/zshenv $HOME/.zshenv"
-ln -fns "$DOTDIR"/zshenv "$HOME"/.zshenv
+  echo "[install.sh] ln -fns $DOTDIR/zshenv $HOME/.zshenv"
+  ln -fns "$DOTDIR"/zshenv "$HOME"/.zshenv
 
-(
-  for d in "$DOTDIR"/config/*; do
-    echo "[install.sh] ln -fns $d $XDG_CONFIG_HOME/$(basename $d)"
-    ln -fns "$d" "$XDG_CONFIG_HOME"/"$(basename "$d")"
-  done
-)
+  (
+    for d in "$DOTDIR"/config/*; do
+      echo "[install.sh] ln -fns $d $XDG_CONFIG_HOME/$(basename "$d")"
+      ln -fns "$d" "$XDG_CONFIG_HOME"/"$(basename "$d")"
+    done
+  )
+}
 
-# WSL
-if [ "$IS_WSL" = true ]; then
+create_wsl_symbolic_links() {
   echo "[install.sh] ln -fns $DOTDIR/local/share/wsl $XDG_DATA_HOME/wsl"
   ln -fns "$DOTDIR"/local/share/wsl "$XDG_DATA_HOME"/wsl
+}
 
-  # Create `/etc/wsl.con`
-  if command -v sudo >/dev/null 2>&1; then
-    echo "[install.sh] sudo cp $DOTDIR/etc/wsl.conf /etc/wsl.conf"
-    sudo cp "$DOTDIR"/etc/wsl.conf /etc/wsl.conf
-  else
-    echo "[install.sh] cp $DOTDIR/etc/wsl.conf /etc/wsl.conf"
-    cp "$DOTDIR"/etc/wsl.conf /etc/wsl.conf
-  fi
+set_variables
+
+show_info
+
+create_xdg_base_directories
+
+set_xdg_base_directories
+
+create_symbolic_links
+
+if [ "$IS_WSL" = true ]; then
+  create_wsl_symbolic_links
 fi
