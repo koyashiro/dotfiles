c86cd57c2b9acbf9e3af212f4061ab6951bbc064
koyashiro <6698252+koyashiro@users.noreply.github.com>
2023-06-21 02:30:20 +0900

    config(neovim): update lualine config

--- shared/.config/nvim/lua/config/lualine.lua
+++ shared/.config/nvim/lua/config/lualine.lua
@@ -1,14 +1,75 @@
 return function()
   require("lualine").setup({
+    options = {
+      icons_enabled = true,
+      theme = "auto",
+      component_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
+      disabled_filetypes = {
+        statusline = {},
+        winbar = {},
+      },
+      ignore_focus = {},
+      always_divide_middle = true,
+      globalstatus = true,
+      refresh = {
+        statusline = 1000,
+        tabline = 1000,
+        winbar = 1000,
+      },
+    },
     sections = {
-      lualine_c = {
+      lualine_a = { "mode" },
+      lualine_b = {
+        "branch",
+        "diff",
+      },
+      lualine_c = { { "filename", path = 1 } },
+      lualine_x = {
         {
           "diagnostics",
           sources = { "nvim_diagnostic", "nvim_lsp" },
           sections = { "error", "warn", "info", "hint" },
           symbols = { error = " ", warn = " ", info = " ", hint = "" },
         },
+        "encoding",
+        "fileformat",
+        "filetype",
       },
+      lualine_y = { "progress" },
+      lualine_z = { "location" },
     },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = { "filename" },
+      lualine_x = { "location" },
+      lualine_y = {},
+      lualine_z = {},
+    },
+    tabline = {
+      lualine_a = {
+        {
+          "tabs",
+          mode = 2,
+          fmt = function(name, context)
+            -- Show + if buffer is modified in tab
+            local buflist = vim.fn.tabpagebuflist(context.tabnr)
+            local winnr = vim.fn.tabpagewinnr(context.tabnr)
+            local bufnr = buflist[winnr]
+            local mod = vim.fn.getbufvar(bufnr, "&mod")
+
+            return name .. (mod == 1 and " +" or "")
+          end,
+        },
+      },
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = {},
+    },
+    winbar = {},
+    inactive_winbar = {},
   })
 end
