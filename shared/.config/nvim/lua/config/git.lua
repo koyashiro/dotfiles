d5cfe9db57537b9e85f0186722804da5699d2459
koyashiro <6698252+koyashiro@users.noreply.github.com>
2023-06-21 03:38:35 +0900

    config(neoivim): support for `range` in `GitBrowse` user command

--- shared/.config/nvim/lua/config/git.lua
+++ shared/.config/nvim/lua/config/git.lua
@@ -1,7 +1,8 @@
 return function()
   require("git").setup()
 
-  vim.api.nvim_create_user_command("GitBrowse", function()
-    require("git.browse").open(false)
-  end, {})
+  vim.api.nvim_create_user_command("GitBrowse", function(args)
+    local visual_mode = args.range == 0
+    require("git.browse").open(visual_mode)
+  end, { range = true })
 end
