2d8426f09cd02b5d6fbac99c7a3d63b2143d62ba
koyashiro <6698252+koyashiro@users.noreply.github.com>
2023-06-22 23:12:19 +0900

    config(neovim): migrate to `lazy.nvim`

--- shared/.config/nvim/lua/plugins.lua
+++ shared/.config/nvim/lua/plugins.lua
@@ -1,137 +1,106 @@
-local function ensure_packer()
-  local fn = vim.fn
-  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-    vim.cmd([[packadd packer.nvim]])
-    return true
-  end
-  return false
-end
-
-local packer_bootstrap = ensure_packer()
-
-require("packer").startup(function(use)
-  use("wbthomason/packer.nvim")
-
-  use({
+return {
+  {
     "folke/tokyonight.nvim",
     config = function()
       vim.cmd.colorscheme("tokyonight")
     end,
-  })
-
-  use({
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     config = require("config.treesitter"),
-  })
-
-  use({
+  },
+  {
     "lewis6991/gitsigns.nvim",
     config = function()
       require("gitsigns").setup()
     end,
-  })
-
-  use({
+  },
+  {
     "dinhhuy258/git.nvim",
     config = require("config.git"),
-  })
-
-  use({
+  },
+  {
     "kevinhwang91/nvim-hlslens",
     config = function()
       require("hlslens").setup()
     end,
-  })
-
-  use({
+  },
+  {
     "petertriho/nvim-scrollbar",
-    requires = {
+    dependencies = {
       "kevinhwang91/nvim-hlslens",
       "lewis6991/gitsigns.nvim",
     },
     config = require("config.scrollbar"),
-  })
-
-  use({
+  },
+  {
     "nvim-lualine/lualine.nvim",
-    requires = { "nvim-tree/nvim-web-devicons" },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = require("config.lualine"),
-  })
-
-  use({
+  },
+  {
     "nvim-tree/nvim-tree.lua",
-    requires = "nvim-tree/nvim-web-devicons",
+    dependencies = "nvim-tree/nvim-web-devicons",
     config = require("config.tree"),
-  })
-
-  use({
+  },
+  {
     "windwp/nvim-autopairs",
     config = function()
       require("nvim-autopairs").setup()
     end,
-  })
-
-  use({
+  },
+  {
     "nvim-telescope/telescope.nvim",
-    requires = {
+    dependencies = {
       "nvim-lua/plenary.nvim",
-      { "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
       "nvim-tree/nvim-web-devicons",
     },
     config = require("config.telescope"),
-  })
-
-  use({
+  },
+  {
     "kylechui/nvim-surround",
     config = function()
       require("nvim-surround").setup()
     end,
-  })
-
-  use({
+  },
+  {
     "terrortylor/nvim-comment",
     config = function()
       require("nvim_comment").setup()
     end,
-  })
-
-  use({
+  },
+  {
     "williamboman/mason.nvim",
-    requires = {
+    dependencies = {
       "williamboman/mason-lspconfig.nvim",
       "neovim/nvim-lspconfig",
     },
     config = require("config.mason"),
-  })
-
-  use({
+  },
+  {
     "glepnir/lspsaga.nvim",
-    requires = {
+    dependencies = {
       "nvim-tree/nvim-web-devicons",
       "nvim-treesitter/nvim-treesitter",
     },
-    opt = true,
+    lazy = true,
     event = "LspAttach",
     config = require("config.lspsaga"),
-  })
-
-  use({
+  },
+  {
     "j-hui/fidget.nvim",
     config = function()
       require("fidget").setup()
     end,
-  })
-
-  use({
+  },
+  {
     "jose-elias-alvarez/null-ls.nvim",
     config = require("config.null-ls"),
-  })
-
-  use({
+  },
+  {
     "hrsh7th/nvim-cmp",
-    requires = {
+    dependencies = {
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
@@ -141,9 +110,5 @@ require("packer").startup(function(use)
       "onsails/lspkind.nvim",
     },
     config = require("config.cmp"),
-  })
-
-  if packer_bootstrap then
-    require("packer").sync()
-  end
-end)
+  },
+}
