c5ba479b72aeb90ec314a6962512514f01da0eef
koyashiro <6698252+koyashiro@users.noreply.github.com>
2023-06-23 01:47:34 +0900

    config(neovim): add `lukas-reineke/indent-blankline.nvim`

--- shared/.config/nvim/lua/plugins.lua
+++ shared/.config/nvim/lua/plugins.lua
@@ -114,6 +114,16 @@ return {
     end,
   },
   {
+    "lukas-reineke/indent-blankline.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("indent_blankline").setup({
+        show_current_context = true,
+        show_current_context_start = true,
+      })
+    end,
+  },
+  {
     "nvim-lualine/lualine.nvim",
     dependencies = {
       "nvim-tree/nvim-web-devicons",
