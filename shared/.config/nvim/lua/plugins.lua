f4201795c18600574d96923c7a2ca1efb408b722
koyashiro <6698252+koyashiro@users.noreply.github.com>
2023-07-02 22:58:32 +0900

    config(neovim): set `settings.Lua.checkThirdParty` to `false`
    Ref: <https://github.com/neovim/nvim-lspconfig/issues/1700#issuecomment-1033127328>

--- shared/.config/nvim/lua/plugins.lua
+++ shared/.config/nvim/lua/plugins.lua
@@ -349,6 +349,7 @@ return {
                 },
                 workspace = {
                   library = vim.api.nvim_get_runtime_file("", true),
+                  checkThirdParty = false,
                 },
                 telemetry = {
                   enable = false,
